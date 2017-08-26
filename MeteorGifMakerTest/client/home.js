f60096eb33297db2a8251ffeab963f4fd38bbb1a
abdullah333 <31362479+abdullah333@users.noreply.github.com>
2017-08-25 22:10:43 -0700

    added comments

--- MeteorGifMakerTest/client/home.js
+++ MeteorGifMakerTest/client/home.js
@@ -1,9 +1,10 @@
+// this function will call once user click on submit button
 function onSubmit(event){
 	var time = document.querySelector('#time').value;
 	var gifImage = document.querySelector('#gifImage');
 	Meteor.submit(event,time,gifImage);
 }
-
+// add file will tigger this event and call savefile
 Template.gifmaker.events({
     'change input': Meteor.saveFile
 });
